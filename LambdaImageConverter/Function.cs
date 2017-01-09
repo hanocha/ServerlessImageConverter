66e891bea88887d9f2178ea227f7dd8d1b00108a
hanocha <saiki@feedforce.jp>
2017-01-09 22:47:00 +0900

    initial commit

--- LambdaImageConverter/Function.cs
+++ LambdaImageConverter/Function.cs
@@ -1,11 +1,12 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
+using ImageSharp;
 using System.Threading.Tasks;
 
+using Amazon.S3;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using System.IO;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -27,9 +28,11 @@ namespace LambdaImageConverter
         /// </summary>
         /// <param name="request"></param>
         /// <returns>The list of blogs</returns>
-        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
+        public async Task<APIGatewayProxyResponse> Get(APIGatewayProxyRequest request, ILambdaContext context)
         {
             context.Logger.LogLine("Get Request\n");
+            Image testImage = await CreateImage(context);
+            context.Logger.LogLine(testImage.ToString());
 
             var response = new APIGatewayProxyResponse
             {
@@ -39,6 +42,22 @@ namespace LambdaImageConverter
             };
 
             return response;
+        }
+
+        private async Task<Image> CreateImage(ILambdaContext context)
+        {
+            var s3Client = new AmazonS3Client();
+            context.Logger.LogLine("Downloading image from S3...");
+            var obj = await s3Client.GetObjectAsync("lambda-image-converter", "lena.jpg");
+            context.Logger.LogLine("Image Download completed.");
+
+            await obj.WriteResponseStreamToFileAsync("/tmp/lena.jpg", false, new System.Threading.CancellationToken());
+
+            context.Logger.LogLine("Image reading...");
+            Image img = new Image(File.OpenRead("/tmp/lena.jpg"));
+            context.Logger.LogLine("Image loaded.");
+
+            return img;
         }
     }
 }
